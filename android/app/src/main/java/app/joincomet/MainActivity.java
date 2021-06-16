c683dd54663cf49cc303116b605dcc07bacb4a8b
Sean Norwood <norwood.sean@gmail.com>
2021-06-16 00:19:43 -0500

    chore: initial

---
+++ android/app/src/main/java/app/joincomet/MainActivity.java
@@ -0,0 +1,5 @@
+package app.joincomet;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'app.joincomet',
+  appName: 'comet',
+  webDir: 'web/dist',
+  bundledWebRuntime: false
+};
+
+export default config;
